636b07998e456e616c31ac20e3601057651607bc
Parham Alvani <parham.alvani@gmail.com>
2024-12-08 13:09:46 +0330

    Squashed 'scripts/lib/' content from commit f5c486e
    git-subtree-dir: scripts/lib
    git-subtree-split: f5c486eddccb97ec22b7009fb53504d6015c1916

---
+++ main.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# aggregate all scripts and load them in one place.
+
+source="$0"
+if [[ -n "${BASH_SOURCE[0]}" ]]; then
+    source="${BASH_SOURCE[0]}"
+fi
+
+dotfile_lib_root="$(cd "$(dirname "${source}")" && pwd)"
+
+# shellcheck source=message.sh
+source "${dotfile_lib_root}/message.sh"
+# shellcheck source=proxy.sh
+source "${dotfile_lib_root}/proxy.sh"
+# shellcheck source=semver.sh
+source "${dotfile_lib_root}/semver.sh"
+if [[ -n "${root}" ]]; then
+    # shellcheck source=linker.sh
+    source "${dotfile_lib_root}/linker.sh"
+fi
+# shellcheck source=require.sh
+source "${dotfile_lib_root}/require.sh"
+# shellcheck source=run.sh
+source "${dotfile_lib_root}/run.sh"
---
+++ tests/test_require_host.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# shellcheck source=main.sh
+source "${root}/../main.sh"
+
+test_require_host_success() {
+    assert_retval require_host "github.com" 0
+}
+
+test_require_host_failed() {
+    assert_retval require_host "github.home" 1
+}
+
+# shellcheck source=unit.sh
+source "${root}/../unit.sh"
