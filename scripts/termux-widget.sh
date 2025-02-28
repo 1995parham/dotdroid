cdc736395d0729f5d8e62a957426fd1b70c65be9
Parham Alvani <parham.alvani@gmail.com>
2025-02-28 07:01:10 +0000

    feat: add installation script

--- scripts/termux-widget.sh
+++ scripts/termux-widget.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 usage() {
-	echo "Launch Termux commands from the homescreen"
+  echo "Launch Termux commands from the homescreen"
 
-	# shellcheck disable=1004,2016
-	echo '
+  # shellcheck disable=1004,2016
+  echo '
  _                                              _     _            _
 | |_ ___ _ __ _ __ ___  _   ___  __   __      _(_) __| | __ _  ___| |_
 | __/ _ \ |__| |_ ` _ \| | | \ \/ /___\ \ /\ / / |/ _` |/ _` |/ _ \ __|
@@ -14,35 +14,36 @@ usage() {
 }
 
 pre_main() {
-	return 0
+  return 0
 }
 
 main_pacman() {
-	return 1
+  return 1
 }
 
 main_xbps() {
-	return 1
+  return 1
 }
 
 main_apt() {
-	return 1
+  return 1
 }
 
 main_pkg() {
-	mkdir "$HOME/.shortcuts" || true
-	copycat "termux-widget" "termux-widget/shortcuts/org-sync.sh" "$HOME/.shortcuts/org-sync.sh" 0
-	copycat "termux-widget" "termux-widget/shortcuts/start-sshd.sh" "$HOME/.shortcuts/start-sshd.sh" 0
+  mkdir "$HOME/.shortcuts" || true
+  copycat "termux-widget" "termux-widget/shortcuts/org-sync.sh" "$HOME/.shortcuts/org-sync.sh" 0
+  copycat "termux-widget" "termux-widget/shortcuts/documents-sync.sh" "$HOME/.shortcuts/documents-sync.sh" 0
+  copycat "termux-widget" "termux-widget/shortcuts/start-sshd.sh" "$HOME/.shortcuts/start-sshd.sh" 0
 }
 
 main_brew() {
-	return 1
+  return 1
 }
 
 main() {
-	return 0
+  return 0
 }
 
 main_parham() {
-	return 0
+  return 0
 }
